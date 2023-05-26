2e09fe713862b50c52627d4776d5a0ab527df9b2
raikustar <134680487+raikustar@users.noreply.github.com>
2023-05-26 21:49:51 +0300

    wordly-0.13v
    update

--- wordleMain.cs
+++ wordleMain.cs
@@ -6,12 +6,16 @@ using TMPro;
 
 public class wordleMain : MonoBehaviour
 {
-    string word = "farts";
+    string[] wordList = { "abuse", "above", "abyss", "acres", "acorn", "adapt", "bless", "blood", "blush", "board", "bogey", "brawl", "bravo"};
+    string word;
     public string guessWord;
 
+
+    // try for loop in input logic
     string[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 
     public rowScript[] row = new rowScript[6];
+    public keyBoardLayout keyBoard; 
     Color my_Green = new Color(0.3f, 0.8f, 0.5f, 1);
     Color my_Default = new Color(0.8f, 0.8f, 0.8f, 1f);
     Color my_Yellow = new Color(1f, 0.9f, 0.4f, 1f);
@@ -20,24 +24,24 @@ public class wordleMain : MonoBehaviour
     int maxAttempt = 6;
     int startRow = 0;
     int newRow;
-    bool gameEnd;
+    bool gameEnd, cor;
 
     private void Start()
     {
         gameEnd = false;
+        cor = false;
+        word = wordList[rng()];
     }
     void Update()
     {
-        Debug.Log(guessWord);
         goToNewRow();
         keyBoardInput();
         unCheckedCompareWords();
-    }
 
+    }
 
     void compareWords()
     {
-
         // For loop to check char position
         for (int i = 0; i<5; i++)
         {
@@ -51,14 +55,13 @@ public class wordleMain : MonoBehaviour
     {
         for (int i = 0; i<5;i++)
         {
-            row[startRow].buttons[i].text = ""+ guessWord[i];
+            row[startRow].buttons[i].text = "" + guessWord[i];
         }
     }
 
     // Check if int i = word string array in for loop
     void checkLetterPos(int i)
     {
-        
         if (word[i] == guessWord[i])  // check if i == current position of string
         {
             row[startRow].blocks[i].color = my_Green;
@@ -86,7 +89,8 @@ public class wordleMain : MonoBehaviour
         {
             gameOver();
             Debug.Log("Game Over!");
-        } else if (gameOverCheck() == true && Input.GetKey(KeyCode.Return))
+        }
+        else if (gameOverCheck() == true && Input.GetKey(KeyCode.Return))
         {
             gameOver();
             Debug.Log("Correct Guess!");
@@ -139,159 +143,133 @@ public class wordleMain : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             n = guessWord + "a";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.B))
         {
             n = guessWord + "b";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.C))
         {
             n = guessWord + "c";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             n = guessWord + "d";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
             n = guessWord + "e";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
 
         else if (Input.GetKeyDown(KeyCode.F))
         {
             n = guessWord + "f";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.G))
         {
             n = guessWord + "g";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.H))
         {
             n = guessWord + "h";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.I))
         {
             n = guessWord + "i";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.J))
         {
             n = guessWord + "j";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.K))
         {
             n = guessWord + "k";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.L))
         {
             n = guessWord + "l";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.M))
         {
             n = guessWord + "m";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.N))
         {
             n = guessWord + "n";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.O))
         {
             n = guessWord + "o";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.P))
         {
             n = guessWord + "p";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.Q))
         {
             n = guessWord + "q";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.R))
         {
             n = guessWord + "r";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
             n = guessWord + "s";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.T))
         {
             n = guessWord + "t";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.U))
         {
             n = guessWord + "u";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.V))
         {
             n = guessWord + "v";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
             n = guessWord + "w";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.X))
         {
             n = guessWord + "x";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.Y))
         {
             n = guessWord + "y";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
         else if (Input.GetKeyDown(KeyCode.Z))
         {
             n = guessWord + "z";
-            guessWord = n;
-            return guessWord;
+            return guessWord = n;
         }
 
 
@@ -299,15 +277,15 @@ public class wordleMain : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.Backspace)) {
 
             n = guessWord.Remove(guessWord.Length - 1);
-            guessWord = n;
-            return guessWord;
+            row[startRow].buttons[n.Length].text = string.Empty;
+            return guessWord = n;
         }
 
         removeExtraChar();
         return guessWord;
     }
 
-
+    // When letters go over 5 return back to last 5 
     void removeExtraChar()
     {
         if(guessWord.Length > 4)
@@ -315,4 +293,21 @@ public class wordleMain : MonoBehaviour
             guessWord = guessWord.Substring(0, 5);
         }
     }
+
+    int rng()
+    {
+        int s = Random.Range(0, wordList.Length);
+        return s;
+    }
+
+    // Effect for box resize after letter input
+
+    //public IEnumerator boxEffect(int i)
+    //{
+    //    row[startRow].blocks[i].transform.localScale = new Vector2(1.1f,1.1f);
+    //    yield return new WaitForSeconds(2f);
+    //    row[startRow].blocks[i].transform.localScale = new Vector2(1f, 1f);
+    //    cor = false;
+    //}
+
 }
